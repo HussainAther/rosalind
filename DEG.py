b37f77a5c3a335a9a330339ffc5d389db71a1c6c
Syed Hussain Ather <sather@umail.iu.edu>
2014-07-09 20:20:26 -0400

    Degree Array
    http://rosalind.info/problems/deg/
    
    In an undirected graph, the degree d(u) of a vertex u is the number of neighbors u has, or equivalently, the number of edges incident upon it.
    
    Given: A simple graph with n≤103 vertices in the edge list format.
    
    Return: An array D[1..n] where D[i] is the degree of vertex i.

--- DEG.py
+++ DEG.py
@@ -3,7 +3,7 @@ n=1000
 l={}
 for i in range(1,n+1):
 	l[i]=0
-print l
+print l 
 for i in f.readlines():
 	l[int(i.split(" ")[0].replace("\n", ""))]+=1
 	l[int(i.split(" ")[1].replace("\n", ""))]+=1
