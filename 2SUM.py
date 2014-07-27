dae07afac4e7785ff48d7ffe38436df062f53014
Syed Hussain Ather <sather@umail.iu.edu>
2014-07-27 12:26:53 -0400

    still needs work to find all matches

--- 2SUM.py
+++ 2SUM.py
@@ -5,20 +5,17 @@ l=[]
 for i in f.readlines():	
 	c+=i
 def func(i):
-	a=0
-	for j in enumerate(i.split(" ")):
-		if a==1:
-			return
-		for k in enumerate(i.split(" ")):
-			if k[1][-1]==j[1][-1]:
-				if int(j[1])==int(k[1])*-1:
-					print str(int(j[0])+1)+" "+ str(int(k[0]+1))
-# 					print str(int(k[0])+1) + "number" + str(int(k[1]))
-					return 
-					a+=1
-	if a==0:
-		print "-1"
+    a=0
+    d=""
+    for j in enumerate(i.split(" "), -1):
+        for k in enumerate(i.split(" "), -1):
+            if k[1][-1]==j[1][-1]:
+                if int(j[1])==int(k[1])*-1:
+                    d=(str(int(j[0])+2)+" "+str(int(k[0]+2)))
+                    print d
+        if d!="":
+            print d
+            return
+    print "-1"
 for i in c.split("\n"):
-	func(i)
-
-#  should i add  "and int(j[1])!=int(0)" to exclude zeroes from the matching?
+    func(i)
